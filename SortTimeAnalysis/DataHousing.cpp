f5e77e64cddc86245d6477d0997133c3895cd5eb
shibiwhy <ian.welch19@icloud.com>
2022-12-06 23:40:48 -0500

    begin to fill out DataHousing.cpp

--- SortTimeAnalysis/DataHousing.cpp
+++ SortTimeAnalysis/DataHousing.cpp
@@ -1,1 +1,40 @@
 #include "DataHousing.h"
+
+
+DataHousing::DataHousing() {
+	this->bubbleSort = 0;
+	this->insertionSort = 0;
+	this->selectionSort = 0;
+}
+
+
+void DataHousing::BubbleSort(int* numbersArray) {
+	bool swap = true;
+	while (swap == true) {
+		swap = false;
+		for (int i = 0; i <= this->arrayLength -2; i++) {
+			int j = i + 1;
+			if (numbersArray[i] > numbersArray[j]) {
+				int temp = numbersArray[j];
+				numbersArray[j] = numbersArray[i];
+				numbersArray[i] = temp;
+				swap = false;
+			}
+		}
+	}
+}
+
+
+void DataHousing::InsertionSort(int* numbersArray) {
+
+}
+
+
+void DataHousing::SelectionSort(int* numbersArray) {
+
+}
+
+
+void DataHousing::ReadData() {
+
+}
--- SortTimeAnalysis/DataHousing.h
+++ SortTimeAnalysis/DataHousing.h
@@ -2,4 +2,22 @@
 #define __DATAHOUSING__
 
 
+class DataHousing {
+private:
+	int* bubbleSort;
+	int* insertionSort;
+	int* selectionSort;
+	int arrayLength;
+
+public:
+	// CONSTRUCTOR
+	DataHousing();
+
+
+	void BubbleSort(int*);
+	void InsertionSort(int*);
+	void SelectionSort(int*);
+
+	void ReadData();
+};
 #endif
