8be672d281cefed6600d3115a78a700595ae595f
shibiwhy <ian.welch19@icloud.com>
2022-12-06 22:54:25 -0500

    fill out Timer.h and Timer.cpp

--- SortTimeAnalysis/Timer.cpp
+++ SortTimeAnalysis/Timer.cpp
@@ -1,1 +1,55 @@
 #include "Timer.h"
+
+
+#include <iostream>
+#include <time.h>
+
+
+// CONSTRUCTOR
+Timer::Timer() {
+	this->beginTime = 0;
+	this->endTime = 0;
+	this->elapsedTime = 0;
+	this->secondsElapsed = 0;
+	this->milsElapsed = 0;
+}
+
+
+bool Timer::Start() {
+	if (this->IsTicking()) {
+		return false;
+	}
+	else {
+		this->beginTime = clock();
+		return true;
+	}
+}
+
+
+bool Timer::Stop() {
+	if (this->IsTicking()) {
+		this->endTime = clock();
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+
+bool Timer::IsTicking() {
+	if (this->endTime = NULL) {
+		return false;
+	}
+	else {
+		return true;
+	}
+}
+
+
+void Timer::GetTime() {
+	this->elapsedTime = this->endTime - this->beginTime;
+	this->secondsElapsed = elapsedTime / CLOCKS_PER_SEC;
+	this->milsElapsed = elapsedTime / (CLOCKS_PER_SEC / 1000);
+	std::cout << this->secondsElapsed << "." << this->milsElapsed << " seconds have passed.";
+}
--- SortTimeAnalysis/Timer.h
+++ SortTimeAnalysis/Timer.h
@@ -1,5 +1,28 @@
 #ifndef __TIMER__
 #define __TIMER__
 
+#include <time.h>
 
+class Timer {
+private:
+	clock_t beginTime;
+	clock_t endTime;
+	clock_t elapsedTime;
+	long int secondsElapsed;
+	long int milsElapsed;
+
+public:
+	// CONSTRUCTOR
+	Timer();
+
+
+	bool Start();
+
+	bool Stop();
+
+	bool IsTicking();
+
+	void GetTime();
+
+};
 #endif
